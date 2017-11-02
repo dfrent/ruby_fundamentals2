fd96bd6999a9c61c38d224cced13c82187db33d9
Your Name <you@example.com>
2017-11-02 14:52:20 -0400

    rbv-methods exercise 3

--- rbv-methods.rb
+++ rbv-methods.rb
@@ -10,16 +10,30 @@
 
 # exercise 2
 
-def negative? (my_number)
-  if my_number.to_i <= -1
-  	return true
-  elsif my_number.to_i>= 1
-  	return false 
-  else my_number.to_i == 0
-  	retrun nil
+# def negative? (my_number)
+#   if my_number.to_i <= -1
+#   	return true
+#   elsif my_number.to_i>= 1
+#   	return false 
+#   else my_number.to_i == 0
+#   	retrun nil
+#   end
+# end 
+
+# # puts " hello am i negative? #{negative?(-3)}"
+
+# puts "hello am i negative? #{negative?(4)}"	
+
+# exercise 3
+
+def is_even? (my_number)
+ if my_number.to_i % 2 == 1
+  	return "false"
+  elsif my_number.to_i % 2 == 0
+  	return "true"
   end
-end 
+end
 
-# puts " hello am i negative? #{negative?(-3)}"
+ puts "is your number even? #{is_even?(4)}"
 
-puts "hello am i negative? #{negative?(4)}"	
+  puts "is your number even? #{is_even?(15)}"
